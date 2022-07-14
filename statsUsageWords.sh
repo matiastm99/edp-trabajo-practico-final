6051c82c07ec89b33f3f1818f91785bed2823cc8
Matias Mutuan <matiastm99@gmail.com>
2022-07-14 16:39:53 -0300

    statsUsageWords.sh: "stopwords" se crea con el contenido correcto. Falla el filtrado principal.

---
+++ statsUsageWords.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function filtrador_de_palabras {
+    PALABRAS_FILTRADAS=()
+    for PALABRA in $(cat "$1"); do
+        if [ ${#PALABRA} -lt 4 ]; then
+            PALABRAS_FILTRADAS+=("$PALABRA")
+        fi
+    done < "$1"
+}
+filtrador_de_palabras "$1"
+echo "${PALABRAS_FILTRADAS[@]}" > stopwords
+
+grep -oE '[[:alpha:]]+' "$1" | grep -v stopwords | sort -f | uniq -ic | sort -nr | head -10
+
