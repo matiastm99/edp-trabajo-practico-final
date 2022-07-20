e495be94e01dc8029acabf92c1d240743505eed2
Matias Mutuan <matiastm99@gmail.com>
2022-07-20 17:09:06 -0300

    statsSentences.sh: Soluciona function "promedio". statsWords.sh: Soluciona function "promedio". texto.txt: Texto utilizado para resolver los ejercicios.

--- scripts/statsSentences.sh
+++ scripts/statsSentences.sh
@@ -22,5 +22,20 @@ function la_oracion_mas_corta {
 	echo "La oracion mas corta es: $CORTA"
 }
 
+function promedio_de_longitud {
+    ORACION=0
+    c=0
+    while read -d "." line; do
+		for PALABRA in $line; do
+			ORACION=$((ORACION+1))
+		done
+		c=$((c+1))
+    done < $1
+	
+	PROMEDIO=$((ORACION/c))
+	echo "El promedio de longitud de una oracion de es: $PROMEDIO palabras"
+}
+
 la_oracion_mas_larga $1
 la_oracion_mas_corta $1
+promedio_de_longitud $1
--- scripts/statsWords.sh
+++ scripts/statsWords.sh
@@ -20,5 +20,18 @@ function palabra_mas_corta {
     echo "La palabra mas corta es: $CORTA"
 }
 
+function promedio_de_longitud {
+    LETRAS=0
+	c=0
+    for PALABRA in $(cat $1); do
+        LETRAS=$((LETRAS+${#PALABRA}))
+		c=$((c+1))
+    done
+   	
+	PROMEDIO=$((LETRAS/c))
+    echo "El promedio de longitud de una palabra es de: $PROMEDIO letras"
+}
+
 palabra_mas_larga $1
 palabra_mas_corta $1
+promedio_de_longitud $1
