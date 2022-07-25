ba7710da6dd4a9224d3c250e0db5eee542c876ea
Matias <matiastm99@gmail.com>
2022-07-25 17:26:29 -0300

    findNames.sh: Agrega mensaje. statsUsageWords.sh: Agrega mensaje.

--- app/scripts/findNames.sh
+++ app/scripts/findNames.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Estas son las palabras que cumplen con la condicion de nombre propio (Nnnnn):"
+
 NOMBRE='^[[:upper:]]'
 for PALABRA in $(cat $1); do
 	if [[ $PALABRA =~ $NOMBRE ]]; then
--- app/scripts/statsUsageWords.sh
+++ app/scripts/statsUsageWords.sh
@@ -11,6 +11,7 @@ function filtrador_de_palabras {
 filtrador_de_palabras $1
 printf '%s\n' "${PALABRAS_FILTRADAS[@]}" > stopwords
 
+echo "Esta son las 10 palabras (compuestas por, minimo, 4 letras) mas utilizadas en el texto:"
 grep -oE '[[:alpha:]]+' $1 | grep -vf stopwords | sort -f | uniq -ic | sort -nr | head -10
 
 
